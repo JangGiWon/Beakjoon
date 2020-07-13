bdc72f9d3fca23337ddee1f2ebe27e78b0c15852
JangGiWon <36566286+JangGiWon@users.noreply.github.com>
2020-07-13 18:09:29 +0900

    update
    update

--- 1978.py
+++ 1978.py
@@ -5,7 +5,7 @@ for i in sosu:
     count += 1
     if i != 1:
         for j in range(2, i):
-            if (i / j) == 1:
+            if (i / j) % 1 == 0:
                 count -= 1
                 break
     else:
